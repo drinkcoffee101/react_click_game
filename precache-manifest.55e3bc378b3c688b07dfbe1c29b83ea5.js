c76f513a5cf7aee2f076a73680a375690c639862
drinkcoffee <dcoffie92@gmail.com>
2019-11-14 16:40:46 -0800

    Updates

---
+++ precache-manifest.55e3bc378b3c688b07dfbe1c29b83ea5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc0fcd51c39cf1d32ef1b15870b48ef",
+    "url": "/react_click_game/index.html"
+  },
+  {
+    "revision": "a2853fd0b5a772e0a577",
+    "url": "/react_click_game/static/js/2.41c9eb87.chunk.js"
+  },
+  {
+    "revision": "16eeca05f2178638f17c",
+    "url": "/react_click_game/static/js/main.be34ca6a.chunk.js"
+  },
+  {
+    "revision": "438f0a0c69578da39d05",
+    "url": "/react_click_game/static/js/runtime-main.3abede52.js"
+  }
+]);
